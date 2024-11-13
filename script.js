c2813724a334b3fe624b546e892cc48c0dc5a558
BuildTools <unconfigured@null.spigotmc.org>
2024-11-13 17:51:47 +0100

    first title, glow mouse and improved header

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const glowFollow = document.querySelector('.glow-follow');
+  const size = 1;
+
+  document.addEventListener('mousemove', (e) => {
+      glowFollow.style.visibility = 'visible';
+      const mouseX = e.pageX;
+      const mouseY = e.pageY;
+      glowFollow.style.transform = `translate(${mouseX - size / 2}px, ${mouseY - size / 2}px)`;
+  });
+});
+
 function checkScreenSize() {
     if (window.innerWidth > 1060) {
         var submenu = document.getElementById('submenu');
@@ -18,5 +30,5 @@ function toggleMenu() {
     } else {
       submenu.style.display = 'block';
     }
-  }
+}
   
